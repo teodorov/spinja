94a1405f520b5e4ba8d20d2497c8c7f1716e3906
marcdejonge <marcdejonge@9d2baae3-d58b-6a5b-da77-ef5247e83a11>
2010-06-20 20:47:24 +0000

    Updated to version 0.9.2

--- src/spinja/Version.java
+++ src/spinja/Version.java
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +15,6 @@
 package spinja;
 
 public final class Version {
-    public static String VERSION = "0.9.1";
-    public static String DATE    = "16-Jun-2010";
+	public static String VERSION = "0.9.2";
+	public static String DATE = "20-Jun-2010";
 }
